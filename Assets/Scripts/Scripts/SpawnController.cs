579bb96a6c6b0fb817a609b725b6adc52e421fd6
Sofi Zav <100384002+sofisofi01@users.noreply.github.com>
2024-06-13 00:32:41 +0300

    refactor scripts

---
+++ Assets/Scripts/Scripts/SpawnController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    private int numberCheck;
+    private Vector3 spawnPos = new Vector3(-20f, 0.8f, 0f);
+    private Animator animator;
+
+    public Vector3 SpawnPos { get => spawnPos; set => spawnPos = value; }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberCheck = 1;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Check")
+        {
+            if (numberCheck != Convert.ToInt16(coll.gameObject.name.Substring(gameObject.name.Length - 1)))
+            {
+                SpawnPos = coll.transform.position;
+                numberCheck = Convert.ToInt16(coll.gameObject.name.Substring(gameObject.name.Length - 1));
+            }
+        }
+    }
+
+
+}
